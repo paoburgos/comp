7a1b4db4f122caef8f1674d6a796d50e32ac0146
Paolo Burgos <paoloburgos.dev@gmail.com>
2024-05-22 23:30:01 -0400

    Base layout for login and signup

---
+++ src/app/(public-routes)/layout.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-screen w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">{children}</div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/assets/images/login_house_image.jpg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
