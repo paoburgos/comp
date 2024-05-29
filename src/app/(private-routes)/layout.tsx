1f21869ee218f591f2f7c81715db711169de8180
Paolo Burgos <paoloburgos.dev@gmail.com>
2024-05-29 00:02:22 -0400

    Refactoring base layout

--- src/app/(private-routes)/layout.tsx
+++ src/app/(private-routes)/layout.tsx
@@ -1,20 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import {
-  ChevronLeft,
   Home,
   LineChart,
   Package,
   Package2,
   PanelLeft,
-  PlusCircle,
   Search,
   Settings,
   ShoppingCart,
-  Upload,
   Users2,
 } from "lucide-react";
-import { Badge } from "../../components/ui/badge";
 import { Button } from "../../components/ui/button";
 import {
   DropdownMenu,
@@ -254,26 +250,6 @@ export default function RootLayout({
         {/* SECTION: Main Content */}
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
           <div className="mx-auto grid w-full max-w-[87.5rem] flex-1 auto-rows-max gap-4">
-            {/* SECTION: Title */}
-            <div className="flex items-center gap-4">
-              <Button variant="outline" size="icon" className="h-7 w-7">
-                <ChevronLeft className="h-4 w-4" />
-                <span className="sr-only">Back</span>
-              </Button>
-              <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
-                Pro Controller
-              </h1>
-              <Badge variant="outline" className="ml-auto sm:ml-0">
-                In stock
-              </Badge>
-              <div className="hidden items-center gap-2 md:ml-auto md:flex">
-                <Button variant="outline" size="sm">
-                  Discard
-                </Button>
-                <Button size="sm">Save Product</Button>
-              </div>
-            </div>
-
             {/* md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8 */}
             <div className="grid gap-4">
               <TRPCReactProvider>{children}</TRPCReactProvider>
